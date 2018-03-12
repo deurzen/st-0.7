ca5fd832b78f24e0f6885862334981fbd0a3b921
deurzen <m.vandeurzen@student.ru.nl>
2018-03-13 00:23:23 +0100

    changes font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Dina:pixelsize=10:antialias=true:autohint=true";
+static char font[] = "tewi:pixelsize=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
